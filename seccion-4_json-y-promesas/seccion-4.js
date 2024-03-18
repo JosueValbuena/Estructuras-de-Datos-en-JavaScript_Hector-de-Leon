88234a809a6ade78bdc6ef97b506a40b31b6b074
Josue <joshvlbn@gmail.com>
2024-03-17 21:07:43 -0300

    first commit

---
+++ seccion-4_json-y-promesas/seccion-4.js
@@ -0,0 +1,14 @@
+//JSON
+
+/* {
+    "name": "Josue Valbuena",
+    "email": "email@gmail.com",
+    "skills": [
+        "frontend", 
+        "backend"
+    ],
+    "address": {
+        "city": "Santiago",
+        "country": "Chile"
+    }
+} */
